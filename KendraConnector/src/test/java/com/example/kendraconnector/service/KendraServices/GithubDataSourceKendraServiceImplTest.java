9675a2c5db874fee07d75be1f335a0f8403724f1
parulrajPTG <parul.raj@peopletech.com>
2023-08-31 23:14:30 -0700

    1. Added Unit test case for checkKendraIndexStatus 2. Added Unit test case for createKendraGithubDataSource 3. Other cosmetic changes

--- KendraConnector/src/test/java/com/example/kendraconnector/service/KendraServices/GithubDataSourceKendraServiceImplTest.java
+++ KendraConnector/src/test/java/com/example/kendraconnector/service/KendraServices/GithubDataSourceKendraServiceImplTest.java
@@ -1,20 +1,23 @@
 package com.example.kendraconnector.service.KendraServices;
 
 import com.example.kendraconnector.dto.ResultItemDto;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import software.amazon.awssdk.services.kendra.KendraClient;
+import software.amazon.awssdk.services.kendra.model.CreateDataSourceRequest;
+import software.amazon.awssdk.services.kendra.model.CreateDataSourceResponse;
 import software.amazon.awssdk.services.kendra.model.CreateIndexRequest;
 import software.amazon.awssdk.services.kendra.model.CreateIndexResponse;
+import software.amazon.awssdk.services.kendra.model.DescribeIndexRequest;
+import software.amazon.awssdk.services.kendra.model.DescribeIndexResponse;
 import software.amazon.awssdk.services.kendra.model.GitHubConfiguration;
+import software.amazon.awssdk.services.kendra.model.IndexStatus;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 class GithubDataSourceKendraServiceImplTest {
@@ -33,32 +36,56 @@ class GithubDataSourceKendraServiceImplTest {
     //@BeforeEach , the MockitoAnnotations.initMocks(this): initializing the mock objects and injects them into the test class.
     // ensuring that the mock objects are ready for use in each test method.
     @BeforeEach
-    public void initMocks(){
+    public void initMocks() {
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    void when_createKendraIndexIsCalled_then_getCorrectId(){
+    void when_createKendraIndexIsCalled_then_getCorrectId() {
         //creates a mock CreateIndexResponse with a sample index ID
-        CreateIndexResponse createIndexResponse  = CreateIndexResponse.builder()
-                .id("1234")
+        String indexId = "indexId";
+        CreateIndexResponse createIndexResponse = CreateIndexResponse.builder()
+                .id(indexId)
                 .build();
         //using Mockito's when method to define the behavior of the mockKendraClient when the createIndex method is called with any CreateIndexRequest.
         when(mockKendraClient.createIndex(any(CreateIndexRequest.class))).thenReturn(createIndexResponse);
         ResultItemDto resultItemDto = injectedObject.createKendraIndex("name", "description");
         //assertEquals assertion ensures that the expected and actual values match.
-        assertEquals("1234", resultItemDto.getId());
+        assertEquals(indexId, resultItemDto.getId());
     }
 
     //Test case for checking index description when createKendraIndex is called
     @Test
     void when_createKendraIndexIsCalled_then_getCorrectIndexDescription() {
+        String indexName = "indexName";
+        String indexDescription = "indexDescription";
         CreateIndexResponse createIndexResponse = CreateIndexResponse.builder().build();
         when(mockKendraClient.createIndex(any(CreateIndexRequest.class))).thenReturn(createIndexResponse);
-        ResultItemDto resultItemDto = injectedObject.createKendraIndex("sinka", "my name");
-        assertEquals("my name", resultItemDto.getDescription());
+        ResultItemDto resultItemDto = injectedObject.createKendraIndex(indexName, indexDescription);
+        assertEquals(indexDescription, resultItemDto.getDescription());
     }
 
-    //Test case for checking if kendraIndex exists
+    //test case for checking Kendra Index Status
+    @Test
+    void when_checkKendraIndexStatusIsCalled_then_getIndexStatusAsActive(){
+        String indexId = "indexId";
+        DescribeIndexResponse describeIndexResponse = DescribeIndexResponse.builder().status(IndexStatus.ACTIVE).build();
+        when(mockKendraClient.describeIndex(any(DescribeIndexRequest.class)))
+                .thenReturn(describeIndexResponse);
+        IndexStatus indexStatus = injectedObject.checkKendraIndexStatus(indexId);
+        assertEquals(IndexStatus.ACTIVE, indexStatus);
+    }
+
+    //Test case for creating GitHub data source associated with Kendra Index
+    @Test
+    void when_createKendraGithubDataSourceIsCalled_then_getIndexId() {
+        String indexId = "indexId";
+        CreateDataSourceResponse createDataSourceResponse = CreateDataSourceResponse.builder().id(indexId).build();
+        when(mockKendraClient.createDataSource(any(CreateDataSourceRequest.class))).thenReturn(createDataSourceResponse);
+        String result = injectedObject.createKendraGithubDataSource(indexId);
+        assertEquals(indexId, result);
+    }
+
+
 
 }
